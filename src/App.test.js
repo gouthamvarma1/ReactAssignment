bd360482a93cdd23d70be12cc05cea188fdb67fe
goutham varma <goutham@bluvision.com>
2021-06-12 16:53:01 +0530

    added unit tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,52 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('Unit Test case1', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Taxila Public School Learning Management System/);
   expect(linkElement).toBeInTheDocument();
-});
+}
+
+);
+
+
+//verify Email Address 
+test('Unit Test case2', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Email address/);
+  expect(linkElement).toBeInTheDocument();
+}
+
+);
+
+
+//verify Registration
+test('Unit Test case3', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Registration/);
+  expect(linkElement).toBeInTheDocument();
+}
+
+);
+
+
+
+//verify birthdate 
+test('Unit Test case4', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Birthday/);
+  expect(linkElement).toBeInTheDocument();
+}
+
+);
+
+
+//verify reenterPassword 
+test('Unit Test case5', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Re-enter Password/);
+  expect(linkElement).toBeInTheDocument();
+}
+
+);
+
